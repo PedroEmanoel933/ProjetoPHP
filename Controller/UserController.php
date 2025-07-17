6f9d7a4c6ee3c60a669291bd9d8514b3a06e7701
PedroEmanoel933 <pemanoel067@gmail.com>
2025-07-17 08:53:42 -0300

    feat: Conclusão do UserController.php

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -17,33 +17,53 @@ class UserController{
             if(empty($user_fullname) or empty($email) or empty($password)){
                 return false;
             }
-            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-            return $this->userModel->registerUser($user_fullname, $email, $hashedPassword);
+            // $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            return $this->userModel->registerUser($user_fullname, $email, $password);
 
         } catch (Exception $error){
             echo "Erro ao cadastrar usuário: ". $error->getMessage();
             return false;
         }
     }
+
+    // EMAIL JÁ CADASTRADO?
+    public function checkUserByEmail($email){
+        return $this -> userModel ->getUserByEmail($email);
+    }
+
     // LOGIN DE USUÁRIO
     public function login ($email, $password){
         $user = $this -> userModel ->getUserByEmail($email);
 
-        if($email){
-            if(crypt($password, $user['password'])){
+        if($user && password_verify($password, $user['password'])){
+            /**
+             *  $user = [
+             * "id" => 1,
+             * "user_fullname" => teste,
+             * "email" => teste@example.com,
+             * "password" => lkadgieg132i50328sdçkgng
+             * ]
+             */
+
                 $_SESSION['id'] = $user['id'];
                 $_SESSION['user_fullname'] = $user['user_fullname'];
                 $_SESSION['email'] = $user['email'];
-
+                
                 return true;
-            } else{
-                return false;
-            }
+            
         }
         return false;
     }
     // USUÁRIO LOGADO?
+    public function isloggedIn(){
+        return isset($_SESSION['id']);
+    }
 
     // RESGATAR INFORMAÇÕES
+    public function getUserData($id, $user_fullname, $email){
+        $id = $_SESSION['id'];
+
+        return $this -> userModel -> getUserInfo($id, $user_fullname, $email);  
+    }
 }
 ?>
